53d2aa792ba343070727a5810171e12bce5a8b01
ashamanyb <ashamanyb@gmail.com>
2022-11-18 18:30:55 +0600

    hw06

---
+++ hw06/hw06.js
@@ -0,0 +1,11 @@
+// Task1
+
+// document.getElementById("hide").onclick = function () {
+//   document.getElementById("text1").hidden = true;
+// };
+
+// Task2
+// Ответ 1 2
+
+// Task 3
+// Ответ  pane.firstChild.onclick = () => pane.remove();
